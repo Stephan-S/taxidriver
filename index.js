2671a7e3dd868a60538aa851f8a23809844e6c71
Stephan Schlosser <Stephan910@web.de>
2016-06-23 14:01:52 +0200

    added bullets

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ io.on('connection', function(socket){
   usercount++;
   active = true;
 
-  io.emit('score', {highscore:highscore, name:highscore_name});
-io.emit('houses', houses);
+  	io.emit('score', {highscore:highscore, name:highscore_name});
+	io.emit('houses', houses);
+	io.emit('projectiles', projectiles);
 	
   socket.on('disconnect', function(){
     console.log('user disconnected');
@@ -35,11 +36,16 @@ io.emit('houses', houses);
   });
 
   socket.on("cargo", function(data){
-    console.log("cargo remove: " + data);
-    cargo.splice(data,1);
-    io.emit('data', cargo);
-  });
-  
+		console.log("cargo remove: " + data);
+		cargo.splice(data,1);
+		io.emit('data', cargo);
+	});
+
+	socket.on("projectile", function(projectile){
+		console.log("projectile added");
+		projectiles[projectiles.length] = projectile;
+		io.emit('projectiles', projectiles);
+	});
 
 
   socket.on("GameOver", function(data){
@@ -90,6 +96,7 @@ http.listen(3000, function(){
 var cargo = [];
 var taxis = [];
 var houses = [];
+var projectiles = [];
 
 
 function pos(x,y){
@@ -111,6 +118,7 @@ function act() {
     //console.log("acting");
     generateCargo();
     sendTaxiData();
+	  actProjectiles();
   }
 
   setTimeout(act,20);
@@ -159,6 +167,7 @@ function generateRectHouse(x,y) {
 	}];
 	houses.push(house);
 }
+
 function generateTriangleHouse(x,y){
 	house = [{
 		'x': x+60+Math.random()*(chunksize-streetsize*2),
@@ -238,9 +247,49 @@ function generateCargo() {
     lastCargo = now;
 
     if(cargo.length<45) {
+
+		var pos_x = Math.floor((Math.random() * fieldwidth-40) + 20);
+		var pos_y = Math.floor((Math.random() * fieldheight-40) + 20);
+		var valid_pos = false;
+		while(!valid_pos){
+			valid_pos = true;
+
+			var c1 = [pos_x + 8, pos_y + 8 ];
+			var c2 = [pos_x + 8, pos_y - 8 ];
+			var c3 = [pos_x - 8, pos_y - 8 ];
+			var c4 = [pos_x - 8, pos_y + 8 ];
+			var mycorners = [
+				{ 	x: c1[0],
+					y: c1[1]
+				},
+				{ 	x: c2[0],
+					y: c2[1]
+				},
+				{ 	x: c3[0],
+					y: c3[1]
+				},
+				{ 	x: c4[0],
+					y: c4[1]
+				}
+			];
+
+			for(h=0; h < houses.length; h++){
+
+				var collision = doPolygonsIntersect(mycorners,houses[h]);
+				if(collision){
+					console.log("collide with house");
+					valid_pos = false;
+				}
+
+			}
+			pos_x = Math.floor((Math.random() * fieldwidth-40) + 20);
+			pos_y = Math.floor((Math.random() * fieldheight-40) + 20);
+
+		}
+
     	new_cargo = {
-    	        x: (Math.floor((Math.random() * (fieldwidth - 30)) + 15)),
-    	        y: (Math.floor((Math.random() * (fieldheight - 30)) + 15))
+    	        x: pos_x,
+    	        y: pos_y
     	      };
     	
     	
@@ -253,3 +302,92 @@ function generateCargo() {
   }
 }
 
+function actProjectiles(){
+	for (p = 0; p < projectiles.length; p++){
+
+		projectiles[p].x = projectiles[p].x + (Math.sin(projectiles[p].direction) * projectiles[p].speed * dt);
+		projectiles[p].y = projectiles[p].y + (Math.cos(projectiles[p].direction) * projectiles[p].speed * dt);
+
+		projectiles[p].distance -= (Math.sqrt(Math.pow(Math.sin(projectiles[p].direction) * projectiles[p].speed * dt,2)
+			+ Math.pow(Math.cos(projectiles[p].direction) * projectiles[p].speed * dt ,2)));
+
+		if(projectiles[p].distance < 0){
+			projectiles.splice(p);
+		}
+	}
+}
+
+function rotatePoint(pivot, point, angle) {
+	// Rotate clockwise, angle in radians
+	var x = Math.round((Math.cos(angle) * (point[0] - pivot[0])) -
+			(Math.sin(angle) * (point[1] - pivot[1])) +
+			pivot[0]),
+		y = Math.round((Math.sin(angle) * (point[0] - pivot[0])) +
+			(Math.cos(angle) * (point[1] - pivot[1])) +
+			pivot[1]);
+	return [x, y];
+}
+
+/**
+ * Helper function to determine whether there is an intersection between the two polygons described
+ * by the lists of vertices. Uses the Separating Axis Theorem
+ *
+ * @param a an array of connected points [{x:, y:}, {x:, y:},...] that form a closed polygon
+ * @param b an array of connected points [{x:, y:}, {x:, y:},...] that form a closed polygon
+ * @return true if there is any intersection between the 2 polygons, false otherwise
+ */
+function doPolygonsIntersect (a, b) {
+	var polygons = [a, b];
+	var minA, maxA, projected, i, i1, j, minB, maxB;
+
+	for (i = 0; i < polygons.length; i++) {
+
+		// for each polygon, look at each edge of the polygon, and determine if it separates
+		// the two shapes
+		var polygon = polygons[i];
+		for (i1 = 0; i1 < polygon.length; i1++) {
+
+			// grab 2 vertices to create an edge
+			var i2 = (i1 + 1) % polygon.length;
+			var p1 = polygon[i1];
+			var p2 = polygon[i2];
+
+			// find the line perpendicular to this edge
+			var normal = { x: p2.y - p1.y, y: p1.x - p2.x };
+
+			minA = maxA = undefined;
+			// for each vertex in the first shape, project it onto the line perpendicular to the edge
+			// and keep track of the min and max of these values
+			for (j = 0; j < a.length; j++) {
+				projected = normal.x * a[j].x + normal.y * a[j].y;
+				if ((minA == undefined) || projected < minA) {
+					minA = projected;
+				}
+				if ((maxA == undefined) || projected > maxA) {
+					maxA = projected;
+				}
+			}
+
+			// for each vertex in the second shape, project it onto the line perpendicular to the edge
+			// and keep track of the min and max of these values
+			minB = maxB = undefined;
+			for (j = 0; j < b.length; j++) {
+				projected = normal.x * b[j].x + normal.y * b[j].y;
+				if ((minB == undefined) || projected < minB) {
+					minB = projected;
+				}
+				if ((maxB == undefined) || projected > maxB) {
+					maxB = projected;
+				}
+			}
+
+			// if there is no overlap between the projects, the edge we are looking at separates the two
+			// polygons, and we know there is no overlap
+			if (maxA < minB || maxB < minA) {
+				//CONSOLE("polygons don't intersect!");
+				return false;
+			}
+		}
+	}
+	return true;
+}
